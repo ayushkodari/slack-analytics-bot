2c38b4501a52e7043f4cada0ce089093db26246a
Harshit Joshi <012802HJoshi@users.noreply.github.com>
2025-10-31 01:37:06 +0530

    slack initialization and sending message check

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import express from "express";
 import { configDotenv } from "dotenv";
+import { slack_init,sendSlackMessage } from "./Helper/slack.js";
 
 configDotenv({path:`./.env.${process.env.NODE_ENV || 'development'}` });
 
 const PORT = process.env.PORT;
+const token = process.env.SLACK_BOT_TOKEN;
+const channel = process.env.SLACK_CHANNEL_ID;
 
 const app = express();
 
-app.get('/',(req,res)=>{
+const slack_web =slack_init(token);
+
+app.get('/message',(req,res)=>{
+    const {message} = req.query;
     res.send("Slack Analytics Bot");
+    sendSlackMessage(slack_web,channel,message);
 })
 
 app.listen(PORT, ()=>{
