fc66874bdb8148544b9cfa5636c2ff94640a7ae0
Harshit Joshi <012802HJoshi@users.noreply.github.com>
2025-10-31 11:33:48 +0530

    Slack Slash command working good

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { configDotenv } from "dotenv";
 import { slack_init,sendSlackMessage } from "./Helper/slack.js";
+import bodyParser from "body-parser";
 
 configDotenv({path:`./.env.${process.env.NODE_ENV || 'development'}` });
 
@@ -10,6 +11,8 @@ const channel = process.env.SLACK_CHANNEL_ID;
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 const slack_web =slack_init(token);
 
 app.get('/message',(req,res)=>{
@@ -17,9 +20,10 @@ app.get('/message',(req,res)=>{
     sendSlackMessage(slack_web,channel,message);
 })
 
-app.get("/",(req,res)=>{
- res.send("Slack Analytics Bot");
-})
+app.post("/show-boobs",(req, res) => {
+  console.log("Slash command received:", req.body);
+  res.send("👀 Command received successfully!");
+});
 
 app.listen(PORT, ()=>{
     console.log(`[SERVER]: server listening at port ${PORT}`);
